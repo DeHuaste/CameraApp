e79ebe8485f4f5a0a6cb7b9b0a52e463d3bf60bf
JD <josh.dahlberg@gmail.com>
2017-06-08 18:53:23 -0400

    Added photo support and refactor
    Moar photos as well as better named UI elements and variables

--- app/src/main/java/com/example/jd/cameraapp/CameraAppActivity.java
+++ app/src/main/java/com/example/jd/cameraapp/CameraAppActivity.java
@@ -16,9 +16,12 @@ import java.io.File;
 public class CameraAppActivity extends AppCompatActivity {
 
     private static final int VIDEO_CAPTURE = 101;
+    private static final int PHOTO_CAPTURE = 102;
+
     private Uri fileUri;
 
-    public void startRecording(View view) {
+    public void startVideo(View view) {
+
         File mediaFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/myvideo.mp4");
 
         Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
@@ -28,21 +31,35 @@ public class CameraAppActivity extends AppCompatActivity {
         startActivityForResult(intent, VIDEO_CAPTURE);
     }
 
+    public void startPhoto(View view) {
+
+        File mediaFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/myphoto.jpg");
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = Uri.fromFile(mediaFile);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(intent, PHOTO_CAPTURE);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_app);
 
-        Button recordButton = (Button) findViewById(R.id.recordButton);
+        Button videoButton = (Button) findViewById(R.id.videoButton);
+        Button photoButton = (Button) findViewById(R.id.photoButton);
 
         if(!hasCamera()) {
-            recordButton.setEnabled(false);
+            videoButton.setEnabled(false);
+            photoButton.setEnabled(false);
         }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
 
-        if(requestCode == VIDEO_CAPTURE) {
+        if(requestCode == VIDEO_CAPTURE || requestCode == PHOTO_CAPTURE) {
             if(resultCode == RESULT_OK) {
                 Toast.makeText(this, getResources().getString(R.string.okMessage) + fileUri, Toast.LENGTH_LONG).show();
             } else {
